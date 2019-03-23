094381a3f3f31a46d9d37a2ff1ca2eb6ca0b87c8
Étienne Lévesque <etienne.levesque16@gmail.com>
2019-03-23 12:50:47 -0400

    /articles now returns all the articles

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import * as statusController from "./status";
 import * as authController from "./authentication";
+import * as articleController from "./article";
 
 export class Api {
   public getRouter(): Router {
@@ -9,6 +10,7 @@ export class Api {
     router.get("/status", statusController.getStatus);
     router.post("/auth/createAccount", authController.createAccount);
     router.post("/auth/authenticate", authController.authenticate);
+    router.get("/articles", articleController.getAll);
     return router;
   }
 }
